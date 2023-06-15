72f7cf4319f5045a000262a70cafca4cf2b249f9
Skhulu9812 <siphesokhetye98@gmail.com>
2023-06-15 11:34:46 -0700

    Cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,11 @@
 /**
  * main - Entry point of the program
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-    const char *str = "Programming is like building a multilingual puzzle\n";
-    int length = 52; // Length of the string including the new line character
-
-    puts(str);
-    fprintf(stderr, "(%d chars long)\n", length);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
     return 0;
 }
 
