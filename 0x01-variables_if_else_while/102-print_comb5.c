7db8d5d02b59c6ca5c363ab6febab13ca7faeadb
Skhulu9812 <siphesokhetye98@gmail.com>
2023-06-17 08:22:36 -0700

    Cisfun

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 100; num1++)
+	{
+		for (num2 = num1; num2 < 100; num2++)
+		{
+			/* Print first number */
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+
+			putchar(' ');
+
+			/* Print second number */
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			/* Print comma and space if not the last combination */
+			if (num1 != 99 || num2 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return 0;
+}
