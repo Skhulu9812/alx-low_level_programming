2b9ea1a0973612ebe8893085e3875f88832617f9
Skhulu9812 <siphesokhetye98@gmail.com>
2023-06-17 08:46:22 -0700

    Cisfun

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,24 +9,17 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 100; num1++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = num1; num2 < 100; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			/* Print first number tens digit */
-			putchar((num1 / 10) + '0');
-			/* Print first number units digit */
-			putchar((num1 % 10) + '0');
-
+			putchar(num1 / 10 + '0');
+			putchar(num1 % 10 + '0');
 			putchar(' ');
+			putchar(num2 / 10 + '0');
+			putchar(num2 % 10 + '0');
 
-			/* Print second number tens digit */
-			putchar((num2 / 10) + '0');
-			/* Print second number units digit */
-			putchar((num2 % 10) + '0');
-
-			/* Print comma and space if not the last combination */
-			if (num1 != 99 || num2 != 99)
+			if (num1 != 98 || num2 != 99)
 			{
 				putchar(',');
 				putchar(' ');
@@ -36,5 +29,5 @@ int main(void)
 
 	putchar('\n');
 
-	return (0);
+	return 0;
 }
