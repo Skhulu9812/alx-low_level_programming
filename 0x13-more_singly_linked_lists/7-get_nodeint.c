5f61d87c2076c0903d7f9b6cde7f1f57a90c40b7
Skhulu9812 <siphesokhetye98@gmail.com>
2023-08-01 10:54:06 -0700

    done

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * get_nodeint_at_index - returns the node at a certain index in a linked list
  * @head: first node in the linked list
@@ -11,13 +10,31 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
  unsigned int i = 0;
  listint_t *temp = head;
-
  while (temp && i < index)
  {
  temp = temp->next;
  i++;
  }
-
  return (temp ? temp : NULL);
+}
+=====================================
+8-sum_listint.c
+#include "lists.h"
+/**
+ * sum_listint - calculates the sum of all the data in a listint_t list
+ * @head: first node in the linked list
+ *
+ * Return: resulting sum
+ */
+int sum_listint(listint_t *head)
+{
+ int sum = 0;
+ listint_t *temp = head;
+ while (temp)
+ {
+ sum += temp->n;
+ temp = temp->next;
+ }
+ return (sum);
 }
 
